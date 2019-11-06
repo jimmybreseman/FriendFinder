f202c80e8b0cee2967fdc7260b6bd37f6f6d167e
Jimmy Breseman <breseman9@gmail.com>
2019-11-06 17:16:46 -0600

    old assignment i finshed up and i had zero commits on and i know i need to start commiting every hr so i will work on doing that

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,24 @@
-//Roughly follow the format below.
+var friendMatch = [
+  {
+    name: "Hulk Hogan",
+    photo: "https://imagez.tmz.com/image/0f/4by3/2019/10/29/0fc3a3372b784d2abd31904f1df37e2e_md.jpg",
+    scores: ["2", "2", "2", "2", "2"]
+  }, {
+    name: "The Rock",
+    photo: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/04/The-Rock-920x584.jpg",
+    scores: ["3", "3", "3", "3", "3"]
+  }, {
+    name: "The Big Show",
+    photo: "https://townsquare.media/site/43/files/2013/04/31664_119392118101216_5533143_n.jpg?w=980&q=75 ",
+    scores: ["1", "1", "1", "1", "1"]
+  }, {
+    name: "The Undertaker",
+    photo: "https://tsj101sports.com/wp-content/uploads/2018/10/the_undertaker.jpg ",
+    scores: ["3", "3", "3", "3", "1"]
+  },
 
-//`json
-// {
-//   "name":"Jimmy",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
+];
+
+
+
+module.exports = friendMatch;
